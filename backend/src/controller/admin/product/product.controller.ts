589dab7eb415ad5390f6fdf5fa3dc808a814130c
semilmore <smore@plusonex.com>
2025-03-10 18:04:02 +0530

    created Put and Status update Api for product

--- backend/src/controller/admin/product/product.controller.ts
+++ backend/src/controller/admin/product/product.controller.ts
@@ -37,3 +37,54 @@ export const getAllProducts = expressAsyncHandler(
     }
   },
 );
+
+export const updateProduct = expressAsyncHandler(
+  async (req: Request, res: Response) => {
+    try {
+      const { productId } = req.params;
+
+      const updateProductRecord = await Product.findByIdAndUpdate(
+        { _id: productId },
+        req.body,
+        { new: true },
+      );
+
+      if (updateProductRecord) {
+        res.status(200).send({ response: updateProductRecord });
+      } else {
+        res.status(400).send({ response: "Failed to update product" });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .send({ response: "Server error, failed to updateb product" });
+    }
+  },
+);
+
+export const updateProductStatus = expressAsyncHandler(
+  async (req: Request, res: Response) => {
+    try {
+      const { productId } = req.params;
+      const { status } = req.body;
+
+      const updatedStatus = status === true ? false : true;
+
+      const updateProductStatusRecord = await Product.findByIdAndUpdate(
+        { _id: productId },
+        { status: updatedStatus },
+        { new: true },
+      );
+
+      if (updateProductStatusRecord) {
+        res.status(200).send({ response: updateProductStatusRecord?.status });
+      } else {
+        res.status(400).send({ response: "Failed to update product status" });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .send({ response: "Server error, failed to update product status" });
+    }
+  },
+);
--- backend/src/route/admin/product/product.rout.ts
+++ backend/src/route/admin/product/product.rout.ts
@@ -3,11 +3,26 @@ import verifyToken from "../../../middleware/admin.middleware";
 import {
   createNewProduct,
   getAllProducts,
+  updateProduct,
+  updateProductStatus,
 } from "../../../controller/admin/product/product.controller";
 
 const adminProductRouter = express.Router();
 
 adminProductRouter.get("/", verifyToken, getAllProducts);
+
 adminProductRouter.post("/create", verifyToken, createNewProduct);
+
+adminProductRouter.put(
+  "/update-product/:productId",
+  verifyToken,
+  updateProduct,
+);
+
+adminProductRouter.put(
+  "/update-status/:productId",
+  verifyToken,
+  updateProductStatus,
+);
 
 export default adminProductRouter;
